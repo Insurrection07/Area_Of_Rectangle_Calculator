f32f3681bf257f27ade5e4a6273e338b841b8ab8
Insurrection <87220681+Insurrection-30@users.noreply.github.com>
2021-12-04 22:34:02 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+l = float(input("Enter the length of the rectangle = "))
+b = float(input("Enter the breadth of the rectangle = "))
+area = l*b
+print('Area of the rectangle is : %0 d' %area)
+
+
+
+
